124b507125ba5f90a4103aa52d83da5a2884d878
Ram kumar <111837904+Ram9117GitHub@users.noreply.github.com>
2022-08-24 00:55:08 +0530

    Add files via upload

---
+++ Assignment 8 - While loop/6.py
@@ -0,0 +1,6 @@
+'''Write a python script to print first 10 even natural numbers'''
+i = 2
+while i<=22:
+    print(i,end=' ')
+    i+=2
+    
---
+++ Assignment 8 - While loop/7.py
@@ -0,0 +1,15 @@
+'''Write a python script to print first 10 even natural numbers in reverse order.'''
+i = 2
+while i<=22:
+    print(22-i,end=' ')
+    i+=2
+""""""""and""""""
+i = 22
+while i>0:
+    print(i,end=' ')
+    i-=2
+""""""""and""""""
+i = 22
+while i>=1:
+    print(i,end=' ')
+    i-=2
